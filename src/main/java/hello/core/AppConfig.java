f8a50d4391e1aef0d3f9e3942480d9bc2e58b625
yeonha99 <rhdqn1207@naver.com>
2021-05-02 18:38:51 +0900

    스프링 컨테이너에 등록

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -4,25 +4,30 @@ import hello.core.Order.OrderService;
 import hello.core.Order.OrderServiceimpl;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberMemberRepository;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceimpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceimpl(memberRepository());
     }//생성자 주입
-
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemberMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceimpl(memberRepository(),discountPolicy());
     }//생성자 주입
-
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,16 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceimpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig=new AppConfig();
-        MemberService memberService=appConfig.memberService();
+      //  AppConfig appConfig=new AppConfig();
+       // MemberService memberService=appConfig.memberService();
+        ApplicationContext applicationContext=new AnnotationConfigApplicationContext(AppConfig.class);
+        //스프링컨테이너에 입력 class의 정보를 가지고 구성을 한다.
+        MemberService memberService=applicationContext.getBean("memberService",MemberService.class);
         Member member=new Member(1L,"memA", Grade.VIP);
         memberService.join(member);
         Member ret=memberService.findMember(1L);
